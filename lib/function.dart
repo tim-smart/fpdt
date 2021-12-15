b03c6dcc6fb3c51f28f66deab947717f2faaa338
Tim Smart <tim.smart@arisechurch.com>
2021-12-15 16:10:32 +1300

    Add compose extension

--- lib/function.dart
+++ lib/function.dart
@@ -3,3 +3,7 @@ T identity<T>(T value) => value;
 extension ChainExtension<T> on T {
   R chain<R>(R Function(T value) transform) => transform(this);
 }
+
+extension ComposeExtension<A, B> on B Function(A) {
+  C Function(A a) compose<C>(C Function(B b) f) => (a) => f(this.call(a));
+}
---
+++ test/function_test.dart
@@ -0,0 +1,30 @@
+import 'package:fpdt/function.dart';
+import 'package:fpdt/option.dart' as O;
+import 'package:test/test.dart';
+
+void main() {
+  group('flow', () {
+    test('can compose functions together', () {
+      final g = O
+          .map((int i) => i * 2)
+          .compose(O.flatMap((i) => O.some(i + 2)))
+          .compose(O.filter((i) => i > 3))
+          .compose(O.toNullable);
+
+      expect(g(O.some(1)), 4);
+    });
+  });
+
+  group('chain', () {
+    test('can pass values into functions', () {
+      final r = O
+          .some(1)
+          .chain(O.map((int i) => i * 2))
+          .chain(O.flatMap((i) => O.some(i + 2)))
+          .chain(O.filter((i) => i > 3))
+          .chain(O.toNullable);
+
+      expect(r, 4);
+    });
+  });
+}
