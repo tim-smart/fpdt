713f07db05ed48aac98b4f927e9df23066cfcb83
Tim Smart <tim.smart@arisechurch.com>
2022-02-23 12:07:01 +1300

    Improve riverpod helpers

--- lib/riverpod.dart
+++ lib/riverpod.dart
@@ -1,17 +1,16 @@
 import 'package:fpdt/fpdt.dart';
 
 /// Helper for creating riverpod providers
-SM Function<SM extends StateMachineBase>(SM sm) stateMachineProvider(
-  dynamic ref,
-) =>
-    <SM extends StateMachineBase>(sm) {
-      ref.onDispose(sm.close);
-      return sm;
-    };
+SM stateMachineProvider<SM extends StateMachineBase>(dynamic ref, SM sm) {
+  ref.onDispose(sm.close);
+  return sm;
+}
 
 /// Helper for creating riverpod providers
-S Function<S>(StateMachineBase<S> sm) stateMachineStateProvider(dynamic ref) =>
-    <S>(sm) {
-      ref.onDispose(sm.stream.listen((s) => ref.state = s).cancel);
-      return sm.state;
-    };
+S stateMachineStateProvider<S>(
+  dynamic ref,
+  StateMachineBase<S> sm,
+) {
+  ref.onDispose(sm.stream.listen((s) => ref.state = s).cancel);
+  return sm.state;
+}
