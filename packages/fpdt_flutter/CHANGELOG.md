d9aa7163a62d9efed4e3c090b24414b3306cc42f
Tim Smart <tim.smart@arisechurch.com>
2022-09-29 17:26:03 +1300

    fpdt_flutter: 0.1.2

--- packages/fpdt_flutter/CHANGELOG.md
+++ packages/fpdt_flutter/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.2
+
+- Export atoms.dart
+
 ## 0.1.1
 
 - Add taskEitherAtom
--- packages/fpdt_flutter/lib/fpdt_flutter.dart
+++ packages/fpdt_flutter/lib/fpdt_flutter.dart
@@ -1,5 +1,6 @@
 library fpdt_flutter;
 
+export 'src/atoms.dart';
 export 'src/flutter_op.dart';
 export 'src/future_value.dart';
 export 'src/state_machine.dart';
